196cdc56e67643e7cdc24a438fe0444bbc2362e2
goncamateus <mgm4@cin.ufpe.br>
2017-06-30 12:05:21 -0300

    Adicionado só os parâmetros

--- project.py
+++ project.py
@@ -1,1 +1,13 @@
-x = 1 +1
+import os
+from numpy import *
+from matplotlib.pyplot import *
+from control.matlab import * 
+os.system('clear')
+print("Escreva os parametros do sistema:")
+M1 = input("Massa sobre cada roda do onibus (Massa do onibus em Kg/4): ")
+M2 = input("Massa de cada suspensao (em Kg): ")
+K1 = input("Constante da mola do sistema de suspensao (N/m): ")
+b1 = input("Constante de amortecimento do sistema de suspensao (N.s/m): ")
+K2 = input("Constante de mola do sistema roda-pneu (N/m): ")
+b2 = input("Constante de amortecimento do sistema roda-pneu (N.s/m): ")
+
